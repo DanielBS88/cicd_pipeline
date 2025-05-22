d33e5082ece15c6e8df1a01a2eb3cd4f546f9837
Daniel Brito dos Santos <daniel.brito2@gmail.com>
2025-05-22 02:03:06 -0300

    Update App.js to dynamically render logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+// Dynamically import the logo
+const logo = require('./logo.svg'); // This will use the logo.svg file updated in the pipeline
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Display dynamically updated logo */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to React</h1>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+      </header>
+    </div>
+  );
 }
 
 export default App;
